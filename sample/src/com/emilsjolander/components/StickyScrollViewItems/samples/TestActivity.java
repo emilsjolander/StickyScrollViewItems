99218e42b3f0a193b792e03675d304b5551aca92
Jan Percic <percic.jan@gmail.com>
2015-11-27 15:27:05 +0000

    OnStickyScrollViewListener implemented
    - added OnStickyScrollViewListener for callback on start sticking and
    stop sticking view
    - updated demo app with OnStickyScrollViewListener implementation
    - updated gradle, build tools, support library, version number, compile
    SDK, demo app icon
    and README

--- sample/src/com/emilsjolander/components/StickyScrollViewItems/samples/TestActivity.java
+++ sample/src/com/emilsjolander/components/StickyScrollViewItems/samples/TestActivity.java
@@ -2,38 +2,55 @@ package com.emilsjolander.components.StickyScrollViewItems.samples;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
-import com.emilsjolander.components.StickyScrollViewItems.R;
+import com.emilsjolander.components.StickyScrollViewItems.StickyScrollView;
 
 /**
- * 
  * @author Emil Sj�lander - sjolander.emil@gmail.com
- *
  */
-public class TestActivity extends Activity {
-    /** Called when the activity is first created. */
+public class TestActivity extends Activity implements StickyScrollView.OnStickyScrollViewListener {
+
+    private static final String TAG = "StickyScrollView";
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        findViewById(R.id.mybutton).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(), "hej", Toast.LENGTH_SHORT).show();
-			}
-		});
-	     
-         /**
-          * Below shows setting the scroll view shadow properties programatically.
-          */
-         // StickyScrollView scrollView = (StickyScrollView)
-         // findViewById(R.id.ScrollView);
-         // scrollView.setShadowDrawable(getResources().getDrawable(
-         // R.drawable.sticky_shadow_default));
-         // scrollView.setShadowHeight(height);
+
+        findViewById(R.id.my_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /**
+         * Below sets the listener on start sticking view and stop sticking current view
+         */
+        StickyScrollView scrollView = (StickyScrollView) findViewById(R.id.scroll_view);
+        scrollView.setOnStickyScrollViewListener(this);
+
+        /**
+         * Below shows setting the scroll view shadow properties programmatically.
+         */
+        //scrollView.setShadowDrawable(getResources().getDrawable(R.drawable.sticky_shadow_default));
+        //scrollView.setShadowHeight(height);
+    }
+
+    @Override
+    public void startStickingView(View v) {
+        Log.e(TAG, "Start sticking view");
+    }
+
+    @Override
+    public void stopStickingCurrentView(View v) {
+        Log.e(TAG, "Stop sticking current view");
     }
 }
